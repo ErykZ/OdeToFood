a3a698768411d9f72e54dc87e83067a67765189a
PLERZAJ <PLERZAJ@PLKRK-L-R9RTL1T>
2017-01-07 09:22:06 +0100

    ASP.NET Core Fundamentals Models in the MVC Framework Detail a Restaurant

---
+++ src/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+using OdeToFood.ViewModels;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController :Controller
+    {
+        private IGreeter _greeter;
+        private IRestaurantData _restaurantData;
+
+        public HomeController(IRestaurantData restaurantData, IGreeter greeter)
+        {
+            _restaurantData = restaurantData;
+            _greeter = greeter;
+        }
+        public IActionResult Index()
+        {
+            var model = new HomePageViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = _greeter.GetGreeting();
+            return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var model = _restaurantData.Get(id);
+            if (model == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ src/OdeToFood/Services/RestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using OdeToFood.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+        Restaurant Get(int id);
+    }
+       
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant { Id =1, Name="The House of Kobe"},
+                new Restaurant { Id = 2, Name="LJ's and the Kat"},
+                new Restaurant {Id=3, Name="King's Contrivance" }
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _restaurants.FirstOrDefault(r => r.Id == id);
+    }
+
+        List<Restaurant> _restaurants;
+    }
+}
